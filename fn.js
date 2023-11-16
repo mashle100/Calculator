f3947a7dabe015846899f2e7d9dc09992802ad19
mashle100 <baddulareddy20@gmail.com>
2023-11-16 20:36:07 +0530

    version 1.2.0

--- fn.js
+++ fn.js
@@ -1,5 +1,5 @@
-let s=[];
-let ope=[];
+let digit=[];
+let operation=[];
 let b="";
 let op;
 let a="";
@@ -21,47 +21,46 @@ function oper(op){
     }
     else{
         b=b+op;
-        ope.push(op);
-        s.push(Number(a));
+        operation.push(op);
+        digit.push(Number(a));
         a="";
         if (op=='C'){
             up=0;
             sum=0;
             a="";
             b="";
-            s=[];
-            ope=[];
+            digit=[];
+            operation=[];
             val="";
             op="";
             document.getElementById("num").innerHTML=b
         }
         document.getElementById("num").innerHTML=b;
         if(op=="="){
-            sum+=(Number(s[up]));
+            sum+=(Number(digit[up]));
             up+=1;
-            for(let i=0;i<ope.length;i+=1){
-                if (ope[i]=='+'){
-                    sum+=(Number(s[up]));
+            for(let i=0;i<operation.length;i+=1){
+                if (operation[i]=='+'){
+                    sum+=(Number(digit[up]));
                     up+=1;
                     console.log(sum);
                 }
-                else if (ope[i]=='-'){
-                    sum=sum-Number(s[up]);
+                else if (operation[i]=='-'){
+                    sum=sum-Number(digit[up]);
                     up+=1;
                 }
-                else if (ope[i]=='*'){
-                    sum=sum*Number(s[up]);
+                else if (operation[i]=='*'){
+                    sum=sum*Number(digit[up]);
                     up+=1;
                 }
-                else if (ope[i]=='/'){
-                    sum=sum/Number(s[up]);
+                else if (operation[i]=='/'){
+                    sum=sum/Number(digit[up]);
                     up+=1;
                 }
-                else if (ope[i]=='%'){
-                    sum=sum%Number(s[up]);
+                else if (operation[i]=='%'){
+                    sum=sum%Number(digit[up]);
                     up+=1;
-                }
-                    
+                }    
             }
             b+=sum;
             document.getElementById("num").innerHTML=b;
